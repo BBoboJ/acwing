e5a8da5a2c1b0f4e5dde9bdc17aae5d8cde8fd91
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:47:31 +0800

    Update 614. 最大值.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/614. 最大值.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/614. 最大值.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int a, b, c, maxnum;
+    
+    cin >> a >> b >> c;
+    
+    maxnum = (a + b + abs(a - b)) / 2;
+    maxnum = (maxnum + c + abs(maxnum - c)) / 2;
+    
+    cout << maxnum << " eh o maior" << endl;
+    
+    return 0;
+}
