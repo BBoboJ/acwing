9fe84c857d025459c58d9bd5d8f1206ea36657cd
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:20:24 +0800

    Update 653. 钞票.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/653. 钞票.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/653. 钞票.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int money;
+    int cash[] = {100, 50, 20, 10, 5, 2, 1};
+    
+    cin >> money;
+    
+    cout << money << endl;;
+    for(int i = 0; i < 7; i ++ )
+    {
+        cout << money / cash[i] << " nota(s) de R$ " << cash[i] << ",00" << endl;
+        money -= (money / cash[i]) * cash[i];
+    }
+    
+    return 0;
+}
