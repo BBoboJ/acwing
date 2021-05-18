22455176ceccd78b0483b49604605a8d62988c43
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:54:29 +0800

    Update 618. 燃料消耗.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/618. 燃料消耗.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/618. 燃料消耗.cpp
@@ -1,1 +1,14 @@
+#include <cstdio>
 
+int main()
+{
+    long long s, t;
+    
+    scanf("%lld %lld", &s, &t);
+    
+    printf("%.3lf",s * t / 12.0);
+    
+    return 0;
+}
+
+// 数据范围(10^18)超过 int(2*10^9)
