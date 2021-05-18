f0b7621b1a238014d81ca1923d61a34a8bd1620c
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:34:08 +0800

    Update 612. 球的体积.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/612. 球的体积.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/612. 球的体积.cpp
@@ -1,1 +1,14 @@
+#include <cstdio>
 
+const double pi = 3.14159;
+
+int main()
+{
+    int r;
+    
+    scanf("%d", &r);
+    
+    printf("VOLUME = %.3lf\n", 4 / (3.0) * pi * r * r * r);
+    
+    return 0;
+}
