f4217173238e7486c14c9f6aa15ab7818f3343b0
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:06:30 +0800

    Update 609. 工资.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/609. 工资.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/609. 工资.cpp
@@ -1,1 +1,13 @@
+#include <cstdio>
 
+int main()
+{
+    int number, hour;
+    double money;
+    
+    scanf("%d%d%lf", &number, &hour, &money);
+    
+    printf("NUMBER = %d\nSALARY = U$ %.2lf\n", number, money * hour);
+    
+    return 0;
+}
