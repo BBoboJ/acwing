be8ca2e32748b16cf4317c1cde3256ae8bac0dcd
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:31:08 +0800

    Update 611. 简单计算.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/611. 简单计算.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/611. 简单计算.cpp
@@ -1,1 +1,18 @@
+#include <cstdio>
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    int s1, s2;
+    double p1, p2;
+    
+    cin >> n1 >> s1 >> p1;
+    cin >> n2 >> s2 >> p2;
+    
+    printf("VALOR A PAGAR: R$ %.2lf\n", s1 * p1 + s2 * p2);
+    
+    return 0;
+}
