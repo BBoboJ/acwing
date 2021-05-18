386c55c7b8ee8099c0c7eb23d424bd5efed83fd4
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-19 01:18:45 +0800

    Add files via upload

---
+++ 语法基础课/第二讲 判断语句/660. 零食.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main ()
+{
+    int x, y;
+    double p[] = {0, 4, 4.5, 5, 2, 1.5};
+    
+    scanf("%d %d", &x, &y);
+    
+    printf("Total: R$ %.2lf\n", p[x] * y);
+    
+	return 0;
+}
