b4d9994402411db437e6a88c3b19915dfbe85e41
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:44:25 +0800

    Update 610. 工资和奖金.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/610. 工资和奖金.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/610. 工资和奖金.cpp
@@ -1,1 +1,16 @@
+#include <cstdio>
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    string name;
+    double x, y;
+    
+    cin >> name >> x >> y;
+    
+    printf("TOTAL = R$ %.2lf\n", x + 0.15 * y);
+    
+    return 0;
+}
