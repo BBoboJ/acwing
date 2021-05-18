2dcedfd727e5af5d32f85ad44a3669bf8b63a43b
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:41:24 +0800

    Update 607. 平均数2.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/607. 平均数2.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/607. 平均数2.cpp
@@ -1,1 +1,15 @@
+#include <cstdio>
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    printf("MEDIA = %.1lf\n", a * 0.2 + b * 0.3 + c * 0.5);
+    
+    return 0;
+}
