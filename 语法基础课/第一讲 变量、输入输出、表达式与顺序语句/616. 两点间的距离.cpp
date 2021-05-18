2c90944144a044b31574a2e183a8f347ea9c0bc4
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-18 16:15:40 +0800

    Update 616. 两点间的距离.cpp

--- 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/616. 两点间的距离.cpp
+++ 语法基础课/第一讲 变量、输入输出、表达式与顺序语句/616. 两点间的距离.cpp
@@ -1,1 +1,17 @@
+#include <cstdio>
+#include <cmath>
 
+int main()
+{
+    double x1, y1, x2, y2;
+    
+    scanf("%lf%lf", &x1, &y1);
+    scanf("%lf%lf", &x2, &y2);
+    
+    printf("%.4lf\n", sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+    
+    return 0;
+}
+
+// sqrt函数
+// ^ 异或符号
