0dd01d5f32bd46643eec758a6eef2da6331f567d
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-23 01:24:52 +0800

    Update and rename 语法基础课/第二讲 判断语句/667. 游戏时间.cpp to 语法基础课/第2讲 判断语句/667. 游戏时间.cpp

--- 语法基础课/第2讲 判断语句/667. 游戏时间.cpp
+++ 语法基础课/第2讲 判断语句/667. 游戏时间.cpp
@@ -3,10 +3,9 @@
 
 using namespace std;
 
-int main ()
+int main()
 {
     int a, b;
-    
     cin >> a >> b;
     
     cout << "O JOGO DUROU "<< (b - a <= 0 ? b - a + 24 : b - a) << " HORA(S)" << endl;
