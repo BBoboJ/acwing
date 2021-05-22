8581ddc472cdae7a52aa947698156a3c6662c07f
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-23 01:32:29 +0800

    Update 714. 连续奇数的和 1.cpp

--- 语法基础课/第3讲 循环语句/714. 连续奇数的和 1.cpp
+++ 语法基础课/第3讲 循环语句/714. 连续奇数的和 1.cpp
@@ -2,14 +2,13 @@
 
 using namespace std;
 
-int main ()
+int main()
 {
     int x, y;
-    int sum = 0;
-    
     cin >> x >> y;
     if (x > y) swap(x, y);
-    
+	
+    int sum = 0;
     while ( ++ x < y)
         if (x % 2)
             sum += x;
