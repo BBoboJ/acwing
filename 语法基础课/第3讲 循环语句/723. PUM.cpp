440050d8e9d089e1d062ea6028ad155ce3c86128
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-23 01:34:36 +0800

    Update 723. PUM.cpp

--- 语法基础课/第3讲 循环语句/723. PUM.cpp
+++ 语法基础课/第3讲 循环语句/723. PUM.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-int main ()
+int main()
 {
-    int n, m, k = 1;
-    
+    int n, m;
     cin >> n >> m;
     
-    for (int i = 0; i < n; i ++ )
+    for (int i = 0, k = 1; i < n; i ++ )
     {
         for (int j = 0; j < m - 1; j ++ )
             cout << k ++ << " ";
