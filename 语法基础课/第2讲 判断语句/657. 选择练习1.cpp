61f90801c2923cd81ba6886be4d2d804f29bbf4d
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-23 01:22:29 +0800

    Update and rename 语法基础课/第二讲 判断语句/657. 选择练习1.cpp to 语法基础课/第2讲 判断语句/657. 选择练习1.cpp

--- 语法基础课/第2讲 判断语句/657. 选择练习1.cpp
+++ 语法基础课/第2讲 判断语句/657. 选择练习1.cpp
@@ -2,10 +2,9 @@
 
 using namespace std;
 
-int main ()
+int main()
 {
     int a, b, c, d;
-    
     cin >> a >> b >> c >> d;
     
     if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0)
