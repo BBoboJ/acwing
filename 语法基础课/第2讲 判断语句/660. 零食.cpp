245f9391f84d54fd5362b2a6d9a71301890e6866
BoboJ <84343635+BBoboJ@users.noreply.github.com>
2021-05-23 01:23:12 +0800

    Update and rename 语法基础课/第二讲 判断语句/660. 零食.cpp to 语法基础课/第2讲 判断语句/660. 零食.cpp

--- 语法基础课/第2讲 判断语句/660. 零食.cpp
+++ 语法基础课/第2讲 判断语句/660. 零食.cpp
@@ -1,10 +1,9 @@
 #include <cstdio>
 
-int main ()
+int main()
 {
     int x, y;
     double p[] = {0, 4, 4.5, 5, 2, 1.5};
-    
     scanf("%d %d", &x, &y);
     
     printf("Total: R$ %.2lf\n", p[x] * y);
